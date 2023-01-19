d4772b18c7e0019ee2820bc18d65e1ca1ef24d82
hassedanilo <hassedanilo@gmail.com>
2023-01-19 16:09:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def hello():
     return "Hello, World!"
+@app.route('/templates')
+def index():
     return_template('index.html')
     
