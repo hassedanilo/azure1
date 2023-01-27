0e67539f94af64a44b288d901c7907f1f04449db
hassedanilo <hassedanilo@gmail.com>
2023-01-27 14:44:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-  return render_template('home.html')
+  return render_template('index.html')
 
 @app.route('/teste')
 def teste():
@@ -30,6 +30,10 @@ def contatos():
 @app.route('/contato')
 def contato():
   return render_template('contato.html')
+
+@app.rout('/home')
+def home():
+  return render_template('home.html')
 
 if __name__ == "__main__":
  app.run(debug=True)
