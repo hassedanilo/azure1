b303843664a01322d54766d66c6c9b3eb53df274
hassedanilo <hassedanilo@gmail.com>
2023-01-31 14:54:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -30,5 +30,9 @@ def contatos():
 def home():
   return render_template('home.html')
 
+@app.route('/contato')
+def contato():
+  return render_template('contato.html')
+
 if __name__ == "__main__":
  app.run(debug=True)
