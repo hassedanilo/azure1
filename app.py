ca0ed05473108718f72c4e5dfa93eeefe4861f3f
hassedanilo <hassedanilo@gmail.com>
2023-02-07 16:29:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -44,8 +44,7 @@ def usuarios():
 
 m = folium.Map(location=[-27.695489, -48.465843], zoom_start=15)
 
-m.save('my_map.html')
-m.save('templates/my_map.html')
+m.save('templates/my_map2.html')
 
 @app.route('/mapa')
 def mapa():
@@ -53,9 +52,9 @@ def mapa():
     folium_map = folium.Map(location=start_coords, zoom_start=14)
     return folium_map._repr_html_()
 
-@app.route('/mapa-arq')
+@app.route('/mapa-arq2')
 def mapa_arq():
-    return render_template('my_map.html')
+    return render_template('my_map2.html')
 
 
 if __name__ == "__main__":
