c0991370853bf22c50f909e1ea89bf7ebea12fae
hassedanilo <hassedanilo@gmail.com>
2023-02-16 13:57:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -75,13 +75,6 @@ def mapa():
    
     HeatMap(list(zip(lat, lng))).add_to(folium_map)
 
-    tooltip = "Click Para Mais Informação"
-    marker = folium.Marker(
-    location=[-27.695489, -48.465843],
-    icon=folium.Icon(icon="cloud"),
-    popup="<stong>Ilha do Campeche</stong>",
-    tooltip=tooltip)
-    marker.add_to(folium_map)
     
     return folium_map._repr_html_()
 
